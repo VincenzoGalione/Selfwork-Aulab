cc8bb4cc65bf5c234b5215d7cd3a9505ef5cfcda
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-24 16:48:50 +0200

    Add files via upload

--- javascript/selfwork-operatori-3/main.js
+++ javascript/selfwork-operatori-3/main.js
@@ -2,20 +2,20 @@ let dato1 = parseInt(prompt('inserisci il primo numero'))
 let dato2 = parseInt(prompt('inserisci il secondo numero'))
 
 let somma = dato1 + dato2
-console.log(somma)
+
 
 let sottrazione = dato1 - dato2
-console.log(sottrazione)
+
 
 let moltiplicazione=dato1 * dato2
-console.log(moltiplicazione)
+
 
 let divisione = dato1 / dato2
-console.log(divisione)
+
 
 let potenza = dato1 ** dato2
-console.log(potenza)
 
-console.log ('Con i numeri da te scelti, i risultati delle varie operazioni sono');
 
-console.log(` somma = ${somma}, sottrazione = ${sottrazione}, moltiplicazione = ${moltiplicazione},divisione = ${divisione},potenza = ${potenza},`)
+console.log ('Con i numeri da te scelti, i risultati delle varie operazioni sono:');
+
+console.log(`somma = ${somma}, sottrazione = ${sottrazione}, moltiplicazione = ${moltiplicazione}, divisione = ${divisione}, potenza = ${potenza},`)
