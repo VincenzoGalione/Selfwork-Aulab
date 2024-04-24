7f6f8c7ebc443771367dc5ac2681579e3d0243c1
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-24 17:21:12 +0200

    selfwork-operatori-2

---
+++ javascript/selfwork-operatori-2/main.js
@@ -0,0 +1,11 @@
+let annocorrente=2024;
+let annodinascita=1995;
+
+let etàpersona=annocorrente-annodinascita;
+console.log(etàpersona);
+
+
+let annimancanti=100-etàpersona;
+console.log(annimancanti);
+
+console.log(`Hai ${etàpersona} anni e ti mancano ${annimancanti} anni per compierne 100`)
