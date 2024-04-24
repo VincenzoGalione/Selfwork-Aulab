7752d48d951ea15c28b6479b0c87fa111586bc7c
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-24 17:14:07 +0200

    selfwork-operatori-1

---
+++ javascript/selfwork-operatori-1/main.js
@@ -0,0 +1,13 @@
+let a=7;
+let b=8;
+let c=9;
+let d=10;
+let e=14;
+
+let somma=a+b+c+d+e;
+console.log(somma);
+
+let media= somma/5;
+console.log(media)
+
+console.log(`(somma) = ${somma}, (media) = ${media}`)
