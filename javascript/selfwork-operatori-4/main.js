e07370cfbad3fbd0960f889f2e3714247e5d572e
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-24 18:00:47 +0200

    selfwork-operatori-4

---
+++ javascript/selfwork-operatori-4/main.js
@@ -0,0 +1,10 @@
+let totgatti = 120
+let filagatti = 6
+let gatti_mancanti = 6
+
+let n_file = totgatti / filagatti
+
+let gatti_avanzo = totgatti % filagatti
+
+
+console.log(`Ci sono ${n_file} file di gatti e ne mancano ${gatti_mancanti} per una nuova fila, con un avanzo di ${gatti_avanzo} gatti`)
