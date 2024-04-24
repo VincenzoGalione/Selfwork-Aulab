629e83b2f7bfe475f50e33fb29470a88187dd4c8
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-24 18:40:15 +0200

    Add files via upload

--- javascript/selfwork-operatori-4/main.js
+++ javascript/selfwork-operatori-4/main.js
@@ -1,10 +1,13 @@
-let totgatti = 120
-let filagatti = 6
-let gatti_mancanti = 6
+let totgatti = 123
+let filagatti = 4
 
-let n_file = totgatti / filagatti
+let n_file =Math.floor(totgatti / filagatti)
+console.log(n_file)
 
 let gatti_avanzo = totgatti % filagatti
+console.log(gatti_avanzo)
 
+let gatti_mancanti = filagatti - gatti_avanzo;
+console.log(gatti_mancanti)
 
 console.log(`Ci sono ${n_file} file di gatti e ne mancano ${gatti_mancanti} per una nuova fila, con un avanzo di ${gatti_avanzo} gatti`)
