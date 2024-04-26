c37eb3e82a1f1692ad30a6cf96efb374a38901e9
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-26 17:17:45 +0200

    Add files via upload

---
+++ javascript/selfwork-cicli-2/main.js
@@ -0,0 +1,19 @@
+let numero;
+let pari = 0;
+let dispari = 0;
+
+
+
+for (let i = 1; i <= 20; i++) {
+         pari = i % 2
+         if (pari ===0) {
+            console.log(pari);
+         }else {
+            console.log(dispari);
+         }
+          
+         
+       }
+   
+
+
