273e959fa4bfa95e7c26956e1d9f5c16cd7db6be
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-26 18:38:11 +0200

    selfwork-cicli-3

---
+++ javascript/selfwork-cicli-3/main.js
@@ -0,0 +1,11 @@
+let bevande = prompt('1 Acqua \n 2 Coca-cola \n 3 Birra');
+if (bevande == 1) {
+    console.log('E’ stata selezionata l’acqua');
+}else if (bevande == 2) {
+     console.log('E’ stata selezionata coca cola');
+} else if (bevande == 3) {
+    console.log('E’ stata selezionata birra');
+}else {
+    prompt(' Attenzione numero non valido!, riprova: \n1 Acqua \n 2 Coca-cola \n 3 Birra')
+}
+   
