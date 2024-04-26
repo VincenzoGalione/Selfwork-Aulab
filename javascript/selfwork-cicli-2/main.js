e38f3ba24d8f1df2c5431df0865f18c612903d51
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-26 17:49:52 +0200

    Add files via upload

--- javascript/selfwork-cicli-2/main.js
+++ javascript/selfwork-cicli-2/main.js
@@ -1,19 +1,19 @@
- let counter = 0
+let counter = 0
 let n_dispari = 0;
 
 
 
 for (let i = 1; i <= 20; i++) {
-         if (i % 2 == 0) {
-            console.log(i);
-         }else{
-            n_dispari = n_dispari + i;
-            counter ++
-         }
+     if (i % 2 == 0) {
+        console.log(i);
+    }else{
+        n_dispari += i;
+        counter ++
+    }
          
-       }
+ }
    
-       console.log(`dispari = ${n_dispari}`);
-       console.log(`counter = ${counter}`);
-       console.log(`media = ${n_dispari/counter}`);
+console.log(`dispari = ${n_dispari}`);
+console.log(`counter = ${counter}`);
+console.log(`media = ${n_dispari/counter}`);
 
