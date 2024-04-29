8690185baf8bf205688a2bd527220eba1633fbd8
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-29 18:35:03 +0200

    selfwork-funzioni-2

---
+++ javascript/selfwork-funzioni-2/main.js
@@ -0,0 +1,22 @@
+function numbers(n) {
+    for (let i = 1 ; i <= n; i++) {
+       console.log(i); 
+    }
+
+    
+    return i;
+}
+
+let nb = i;
+
+if (nb % 3 == 0) {
+    nb = 'Fizz'
+} else if (n % 5 == 0) {
+    nb = 'Buzz'
+}else {
+    
+}
+
+
+  
+let numero = numbers (45);
