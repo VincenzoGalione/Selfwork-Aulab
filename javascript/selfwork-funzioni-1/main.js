5880f29a84131b3fbbbb6c2b2b2d7be97e0aa732
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-29 17:46:30 +0200

    selfwork-funzioni-1

---
+++ javascript/selfwork-funzioni-1/main.js
@@ -0,0 +1,27 @@
+let n = prompt('Quanti tiri effettuare') ;
+
+
+function lanci(giocatore){
+    let punteggio = 0;
+    
+    for (let i = 0; i < n; i++) {
+        punteggio +=Math.floor(Math.random() * (6 - 1) + 1);
+         
+         
+    }
+
+    return punteggio;
+}
+
+
+let player1 = lanci ('Vincenzo');
+console.log(` Il totale di Vincenzo è ${player1}`);
+
+let player2 = lanci ('Matteo');
+console.log(` Il totale di Matteo è ${player2}`);
+
+if (player1 > player2) {
+    console.log(`Il vincitore è Vincenzo`);
+}else {
+    console.log(`Il vincitore è Matteo`);
+}
