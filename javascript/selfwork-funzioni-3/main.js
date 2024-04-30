936227f380130df446809536da0ed09738a93ffc
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-04-30 10:04:47 +0200

    Add files via upload

--- javascript/selfwork-funzioni-3/main.js
+++ javascript/selfwork-funzioni-3/main.js
@@ -1,18 +1,17 @@
  
 function numbers(valori) {
     if (valori < 10) {
-        console.log('Il  è formato da 1 cifra');
+        return ' 1 cifra'
     }else if (valori < 100) {
-        console.log('Il valore  è formato da 2 cifre ');
+        return ' 2 cifre'
     }else if (valori < 1000) {
-        console.log('Il valore  è formato da 3 cifre ');
+        return ' 3 cifre '
     }else if (valori < 10000) {
-        console.log('Il valore  è formato da 4 cifre ');
+        return ' 4 cifre' 
     }else {
-        console.log('Numero troppo grande');
+        return 'numero troppo grande';
     }   
-    return 
+   
 }
 
-
-numbers(12000)
+console.log(numbers(100));
