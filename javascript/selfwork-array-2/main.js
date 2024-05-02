05d57fd6d01b1184683e12806064ceecd169abf6
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-02 17:51:21 +0200

    selfwork-array-2

---
+++ javascript/selfwork-array-2/main.js
@@ -0,0 +1,12 @@
+let numbers= [14, 5, 7, 1, 3,];
+let somma = numbers.reduce((acc,n)=> acc+n);
+console.log(`Il valore della somma è ${somma}`);
+
+let media = somma / 5
+console.log(`Il valore della media è ${media}`);
+
+let maggiori = numbers.filter((numbers)=>numbers > media);
+console.log(`I valori maggiori della media sono : ${maggiori}`);
+
+let minori = numbers.filter((numbers)=>numbers < media);
+console.log(`I valori maggiori della media sono : ${minori}`);
