6d74af245bc3615dd6607841230eebb41b1c3ec7
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-02 16:35:26 +0200

    selfwork-array-1

---
+++ javascript/selfwork-array-1/main.js
@@ -0,0 +1,9 @@
+let number = [3, 7, -2, 14, 8, 1, 2, 5, 6, -1];
+
+
+number.sort((a,b)=> b-a);
+console.log(number,  'ordine  decrescente');
+
+
+number.sort((a,b)=> a-b);
+console.log(number, 'ordine crescente');
