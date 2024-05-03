2658a1f109bba2a5c22848ecc0fc40d6711c96ef
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-03 18:43:47 +0200

    Add files via upload

--- javascript/selfwork-oggetti-1/main.js
+++ javascript/selfwork-oggetti-1/main.js
@@ -1,8 +1,10 @@
 let persona = {
-    'nome' : 'Luca',
-    'cognome' : 'Rossi',
-    'età': 35,
-    
+    nome : 'Luca',
+    cognome : 'Rossi',
+    età: 35,
+    salutare : function(){
+        console.log(`Ciao a tutti sono ${persona.nome} ${this.cognome} e ho ${this.età} anni` );
+    }
 }
-
 console.log(persona);
+persona.salutare()
