5f8a9b233d43285a94591e14aa4e6cb372014899
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-04 12:37:56 +0200

    Add files via upload

--- javascript/selfwork-oggetti-2/main.js
+++ javascript/selfwork-oggetti-2/main.js
@@ -1,32 +1,67 @@
+ let nome = 'Lucia';
+ let telefono = 326451645;
+ 
  let agenda = {
-    'contatti' : [
-    {'nome' : 'Luca', 'telefono': 3331111111},
-    {'nome' : 'Matteo', 'telefono': 3332222222},
-    {'nome' : 'Maria', 'telefono': 33344444444},
-    {'nome' : 'Clara', 'telefono': 33355555555},
+    contatti : [
+    {nome : 'Luca', telefono: 3331111111},
+    {nome : 'Matteo', telefono: 3332222222},
+    {nome : 'Maria', telefono: 33344444444},
+    {nome : 'Clara', telefono: 33355555555},
     ],
 
     mostra_contatti : function () {
-        agenda.contatti.forEach((contatto) => console.log(contatto));
+        this.contatti.forEach(contatto => {
+            console.log(`${contatto.nome}, ${contatto.telefono}`);
+        })
     },
 
-    rimuovi_contatto : function (nome_rimosso){
-        let index = this.contatti.indexOf(nome_rimosso);
-        this.contatti.splice(index,1);
-        console.log(index);   
+    mostra_contatto : function(nome){
+        let contatto = this.contatti.find(contatto => contatto.nome == nome)
+        if (contatto) {
+            console.log(`Il contatto è stato trovato : ${contatto.nome}, ${contatto.telefono}`);
+        }else{
+            console.log('Contatto non trovato');
+        }
     },
 
-    aggiungi_contatto : function(){}
+    rimuovi_aggiungi_contatto : function (nome, telefono){
+        let contatto = this.contatti.find(contatto => contatto.nome == nome)
+        let index = this.contatti.indexOf(contatto);
+        if (contatto) {
+            this.contatti.splice(index, 1)
+            console.log('Il contatto è stato eliminato');
+        }else{
+            this.contatti.push({nome: nome, telefono: telefono})
+            console.log('Il contatto è stato aggiunto');
+        }
+    },
+
+    modifica_contatto : function(nome, nuovo_numero){
+        let contatto = this.contatti.find(contatto => contatto.nome == nome)
+        if (contatto) {
+            console.log('Il contatto è stato modificato');
+            contatto.telefono = nuovo_numero
+        }else{
+            console.log('Contatto non trovato');
+        }
+    }
+    
  }
 
-
-//  console.log(agenda);
 // mostro tutti i contatti
-// agenda.mostra_contatti();
+agenda.mostra_contatti();
 
 // mostro un singolo contatto
-// console.log(agenda.contatti[0]);
-
+agenda.mostra_contatto('Anna')
+ 
 // rimuovi contatto
-agenda.rimuovi_contatto('Maria');
+agenda.rimuovi_aggiungi_contatto('Maria');
+ 
+// aggiungi contatto
+agenda.rimuovi_aggiungi_contatto(nome, telefono)
+ 
+// modifica contatto
+agenda.modifica_contatto('Paolo', 4)
 
+// mostro tutti i contatti
+agenda.mostra_contatti();
