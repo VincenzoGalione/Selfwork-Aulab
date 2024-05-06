03d37652423d9222aa6e9d23c5e74f4aff28ee0f
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-06 17:35:00 +0200

    Add files via upload

--- javascript/selfwork-oggetti-3/main.js
+++ javascript/selfwork-oggetti-3/main.js
@@ -1,50 +1,17 @@
-let bowling = {
-    giocatori : [
-        {'nome' : 'Paolo' , 'punteggi' :[] },
-        {'nome' : 'Luca' , 'punteggi' : [] },
-        {'nome' : 'Dani' , 'punteggi' :[] },
-        {'nome' : 'Eva' , 'punteggi' :[] },
-    ],
-    
-    imposta_punteggi : function(){
-        this.giocatori.forEach(giocatore => {
-            for (let i = 0; i < 10 ; i++) {
-                giocatore.punteggi.push(Math.floor(Math.random() * (10 - 1 +1) + 1),   
-            )   
-        }
-        giocatore.punteggi.sort((a,b)=> b-a);
-         console.log(giocatore);     
-        })
+ 
+function numbers(valori) {
+    if (valori < 10) {
+        return ' 1 cifra'
+    }else if (valori < 100) {
+        return ' 2 cifre'
+    }else if (valori < 1000) {
+        return ' 3 cifre '
+    }else if (valori < 10000) {
+        return ' 4 cifre' 
+    }else {
+        return 'numero troppo grande';
+    }   
    
-    },
-
-    totale_punteggi : function(){
-        this.giocatori.forEach(giocatore =>{
-        let totale_punteggio = giocatore.punteggi.reduce((acc,n)=> acc + n);  
-        console.log(`Il Punteggio totale di ${giocatore.nome} è ${totale_punteggio}`);
-        })
-   
-    },
-
-    aggiungi_giocatore : function(){
-        this.giocatori.push({nome : 'Alex' , 'punteggi': []})
-          this.giocatori.filter((giocatore,i) => {
-            if (this.giocatori.punteggi = 0) {
-                for (let i = 0; i < 10 ; i++) {
-                    giocatore.punteggi.push(Math.floor(Math.random() * (10 - 1 +1) + 1),   
-                )   
-                giocatore.punteggi.sort((a,b)=> b-a);
-                // console.log(giocatore);     
-                
-            }
-        }
-        })
-   
-    },
-    
 }
 
-bowling.imposta_punteggi()
-bowling.totale_punteggi()
-bowling.aggiungi_giocatore()
-console.log(bowling.giocatori);
+console.log(numbers(100));
