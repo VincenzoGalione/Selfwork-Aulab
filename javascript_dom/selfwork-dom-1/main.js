3504d9aea5397f4edba7543d45c2497bcd9e767f
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-05-09 16:20:50 +0200

    Add files via upload

--- javascript_dom/selfwork-dom-1/main.js
+++ javascript_dom/selfwork-dom-1/main.js
@@ -1,15 +1,22 @@
 
 let paragrafi = document.querySelectorAll('p')
-let btnColor = document.querySelector('btnColor')
-let grBtn = document.querySelector('grBtn')
-let noneBtn = document.querySelector('noneBtn')
+let btnColor = document.querySelectorAll('#btnColor')
+let grBtn = document.querySelectorAll('#grBtn')
+let noneBtn = document.querySelectorAll('#noneBtn')
 
 
 btnColor.addEventListener('click', ()=>{
     paragrafi.forEach(paragrafo =>{
-        let r = Math.floor(Math.random() * (256 - 1) + 1);  
-        let g = Math.floor(Math.random() * (256 - 1) + 1);
-        let b = Math.floor(Math.random() * (256 - 1) + 1); 
+        let r = Math.floor(Math.random() * 256);  
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256); 
        paragrafo.style.color = `rgb(${r},${g},${b})`
+    })
+})
+
+
+grBtn.addEventListener('click', ()=>{
+    paragrafi.forEach(paragrafo=>{
+        paragrafo.style.fontWeight = 'bold'
     })
 })
